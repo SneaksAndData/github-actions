52056ffe6a607cec6fc57fd3f400d202afad85a8
Vitaliy Savitskiy <visa@ecco.com>
2022-06-09 14:41:16 +0200

    Make it ready for review

--- semver_release/semver_release.sh
+++ semver_release/semver_release.sh
@@ -7,7 +7,6 @@ currentVersion=$(git describe --tags --abbrev=7)
 currentMinor=$(echo "$currentVersion" | cut --delimiter=. --fields=2)
 currentMajor=$(echo "$currentVersion" | cut --delimiter=. --fields=1 | cut --delimiter=v --fields=2)
 
-echo "Current version: $currentVersion"
 if [[ $currentMajor -eq $providedMajor ]] && [[ $providedMinor -eq $currentMinor ]];
 then
   currentRevision=$(echo "$currentVersion" | rev | cut --delimiter=. --fields=1 | rev | cut --delimiter=- --fields=1)
@@ -16,7 +15,5 @@ else
   nextRevision='0'
 fi
 nextVersion="v$providedMajor.$providedMinor.$nextRevision"
-echo "Next version: $currentVersion"
-#gh release create "$nextVersion" --generate-notes
-
+gh release create "$nextVersion" --generate-notes
 echo "::set-output name=version::$nextVersion"
