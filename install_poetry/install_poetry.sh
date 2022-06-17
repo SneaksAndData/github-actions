dac52c28840cd62f016fb188a42ba8d906438e98
Vitaliy Savitskiy <visa@ecco.com>
2022-06-17 09:50:17 +0200

    Fix

--- install_poetry/install_poetry.sh
+++ install_poetry/install_poetry.sh
@@ -209,7 +209,9 @@ curl -sSL https://install.python-poetry.org | python3 - --preview
 export PATH=/github/home/.local/bin:$PATH
 poetry config repositories.azops "$REPO_URL"
 poetry install
+REQUIREMENTS_PATH="$PWD/.container/requirements.txt "
 if [[ "$(echo "$EXPORT_REQUIREMENTS" | tr '[:upper:]' '[:lower:]')" == "true" ]]; then
-  poetry export -f requirements.txt --output .container/requirements.txt --without-hashes --with-credentials
+  poetry export -f requirements.txt --output $REQUIREMENTS_PATH --without-hashes --with-credentials
+  echo "requirements exported to $REQUIREMENTS_PATH"
 fi
 
