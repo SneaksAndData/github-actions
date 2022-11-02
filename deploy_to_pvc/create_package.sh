864b2d506048bd0ecbeecdad8b220bfbc7ec2f37
Vitaliy Savitskiy <visa@ecco.com>
2022-11-02 11:59:51 +0100

    Fix action name

--- deploy_to_pvc/create_package.sh
+++ deploy_to_pvc/create_package.sh
@@ -34,23 +34,23 @@ az login --service-principal \
   --password "$CLUSTER_SP_CLIENT_PASSWORD" \
   --tenant "$TENANT_ID"
 
-# shellcheck disable=SC2086
-az account set --subscription $SUBSCRIPTION_ID
+az account set --subscription "$SUBSCRIPTION_ID"
 az aks get-credentials --name "$AKS_NAME" --resource-group "$AKS_NAME"
 
 distribution_pvc=$(kubectl get pv --namespace "$NAMESPACE" | grep "$CLAIM_NAME" | cut -d' ' -f1)
-spark_volume_handle=$(kubectl get pv --namespace "$NAMESPACE" "$distribution_pvc" -o json | jq .spec.csi.volumeHandle | cut -d# -f2)
-spark_dist_mount_account_secret=azure-storage-account-$spark_volume_handle-secret
-spark_dist_mount_account=$(kubectl get secret --namespace "$NAMESPACE" "$spark_dist_mount_account_secret" -o json | jq .data.azurestorageaccountname | cut -d'"' -f2 | base64 -d)
-spark_dist_mount_account_key=$(kubectl get secret --namespace "$NAMESPACE" "$spark_dist_mount_account_secret" -o json | jq .data.azurestorageaccountkey | cut -d'"' -f2 | base64 -d)
+volume_handle=$(kubectl get pv --namespace "$NAMESPACE" "$distribution_pvc" -o json | jq .spec.csi.volumeHandle | cut -d# -f2)
+account_secret=azure-storage-account-$volume_handle-secret
+account_name=$(kubectl get secret --namespace "$NAMESPACE" "$account_secret" -o json | jq .data.azurestorageaccountname | cut -d'"' -f2 | base64 -d)
+account_key=$(kubectl get secret --namespace "$NAMESPACE" "$account_secret" -o json | jq .data.azurestorageaccountkey | cut -d'"' -f2 | base64 -d)
 
-destination="https://$spark_dist_mount_account.file.core.windows.net/$distribution_pvc"
-        
+
 echo Generating SAS for upload
 end=$(date -d '+5 minutes' '+%Y-%m-%dT%H:%MZ')
-sas=$(az storage account generate-sas --account-key "$spark_dist_mount_account_key" --account-name "$spark_dist_mount_account" --expiry "$end" --https-only --permissions acdlpruw --resource-types sco --services f | cut -d'"' -f2)
+sas=$(az storage account generate-sas --account-key "$account_key" --account-name "$account_name" --expiry "$end" --https-only --permissions acdlpruw --resource-types sco --services f | cut -d'"' -f2)
 
+destination="https://$account_name.file.core.windows.net/$distribution_pvc"
 authorized_destination="$destination/$PROJECT_NAME/$current_version?$sas"
+
 echo Getting AzCopy
 curl -s -L https://aka.ms/downloadazcopy-v10-linux --output azcopy.tar.gz \
   && tar -xf azcopy.tar.gz -C . --strip-components=1
