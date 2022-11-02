4e6f49bf905f592561eb4de6f7288e9374f46fed
Vitaliy Savitskiy <visa@ecco.com>
2022-11-02 15:47:50 +0100

    Split single action to multiple

--- get_pvc_sas/get_pvc_sas.sh
+++ get_pvc_sas/get_pvc_sas.sh
@@ -16,28 +16,6 @@
 
 set -e
 
-export PATH="/github/home/.local/bin:$PATH"
-
-current_version=$(git describe --tags --abbrev=7)
-
-echo "Preparing upload for $PROJECT_NAME $current_version"
-
-env_path=$(poetry env info | grep Path | head -n 1 | awk '{print $2}')
-         
-mkdir -p "./$DEPLOYMENT_ROOT/$PROJECT_NAME/$current_version/$PROJECT_NAME"
-
-mv -v "$env_path"/lib/python3.9/site-packages/* "./$DEPLOYMENT_ROOT/$PROJECT_NAME/$current_version/"
-mv -v ./"$PROJECT_NAME"/* "./$DEPLOYMENT_ROOT/$PROJECT_NAME/$current_version/$PROJECT_NAME/"
-
-echo 'Getting cluster credentials'
-az login --service-principal \
-  --username "$CLUSTER_SP_CLIENT_ID" \
-  --password "$CLUSTER_SP_CLIENT_PASSWORD" \
-  --tenant "$TENANT_ID"
-
-az account set --subscription "$SUBSCRIPTION_ID"
-az aks get-credentials --name "$AKS_NAME" --resource-group "$AKS_NAME"
-
 distribution_pvc=$(kubectl get pv --namespace "$NAMESPACE" | grep "$CLAIM_NAME" | cut -d' ' -f1)
 volume_handle=$(kubectl get pv --namespace "$NAMESPACE" "$distribution_pvc" -o json | jq .spec.csi.volumeHandle | cut -d# -f2)
 account_secret=azure-storage-account-$volume_handle-secret
@@ -58,15 +36,8 @@ sas=$(
       --services f | cut -d'"' -f2
 )
 
-destination="https://$account_name.file.core.windows.net/$distribution_pvc/$PROJECT_NAME/$current_version"
+destination="https://$account_name.file.core.windows.net/$distribution_pvc/$DIRECTORY_NAME"
 authorized_destination="$destination?$sas"
-
-echo Getting AzCopy
-curl -s -L https://aka.ms/downloadazcopy-v10-linux --output azcopy.tar.gz \
-  && tar -xf azcopy.tar.gz -C . --strip-components=1
-
-echo "Deploying $PROJECT_NAME $current_version to $destination"
-./azcopy copy ./"$DEPLOYMENT_ROOT/$PROJECT_NAME/$current_version/*" "$authorized_destination" \
-  --recursive \
-  --overwrite true \
-  --put-md5
+echo "::add-mask::$sas"
+echo "::add-mask::$authorized_destination"
+echo "authorized_destination=$authorized_destination" >> "$GITHUB_OUTPUT"
