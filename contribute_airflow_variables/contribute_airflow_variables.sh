0ba97a37dac1d82198d8d03994ebf5ecba24eccc
Vitaliy Savitskiy <visa@ecco.com>
2023-01-25 13:56:38 +0100

    Fix variable name

--- contribute_airflow_variables/contribute_airflow_variables.sh
+++ contribute_airflow_variables/contribute_airflow_variables.sh
@@ -23,12 +23,12 @@ do
 
   echo "Waiting for the deployment package to be generated"
   sleep 5
-  is_completed=$(gh workflow view 'Prepare Helm Chart' --repo "$REPO_NAME" | grep "Updating $PROJECT_NAME to $PROJECT_VERSION" | grep main | grep completed || true)
+  is_completed=$(gh workflow view 'Prepare Helm Chart' --repo "$TARGET_REPO_NAME" | grep "Updating $PROJECT_NAME to $PROJECT_VERSION" | grep main | grep completed || true)
 
 done
 
-new_release=$(gh release list --repo "$REPO_NAME" --limit 1 | tail -n 1 | cut -d$'\t' -f1)
+new_release=$(gh release list --repo "$TARGET_REPO_NAME" --limit 1 | tail -n 1 | cut -d$'\t' -f1)
 
 echo "Submitting a deployment for a new configuration release $new_release"
 
-gh workflow run 'Deploy Variables to Airflow' --repo "$REPO_NAME" --field environment="$DEPLOY_ENVIRONMENT" --ref "refs/tags/$new_release"
+gh workflow run 'Deploy Variables to Airflow' --repo "$TARGET_REPO_NAME" --field environment="$DEPLOY_ENVIRONMENT" --ref "refs/tags/$new_release"
