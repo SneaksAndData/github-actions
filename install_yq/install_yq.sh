921f05ba08649ba380f833af09ddbd6aa1190796
Vitalii Savitskii <visa@ecco.com>
2023-02-09 15:14:45 +0100

    Add install_yq action

---
+++ install_yq/install_yq.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+#  Copyright (c) 2022 Ecco Sneaks & Data
+#
+#  Licensed under the Apache License, Version 2.0 (the "License");
+#  you may not use this file except in compliance with the License.
+#  You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+#  Unless required by applicable law or agreed to in writing, software
+#  distributed under the License is distributed on an "AS IS" BASIS,
+#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#  See the License for the specific language governing permissions and
+#  limitations under the License.
+
+set -Eeuo pipefail
+
+echo "Getting AzCopy from $DOWNLOAD_URI"
+curl -s -L "$DOWNLOAD_URI" --output azcopy.tar.gz \
+  && tar xzvf azcopy.tar.gz -C . --strip-components=1
